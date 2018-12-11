a4abe4f4ca36763c457da07c5174ed4d93d18677
Matt Wilcox <mwilcox@lunalights.org>
2018-12-11 15:56:01 -0600

    disconnect from device after id fetch

--- src/components/DeviceId/DeviceIdGetterContainer.js
+++ src/components/DeviceId/DeviceIdGetterContainer.js
@@ -16,7 +16,6 @@ export class DeviceIdGetterContainer extends React.Component {
     this.setState({ header: "your device id:" });
     this.setState({ id: "fetching id..." });
     try {
-      console.log("detected click!");
       const response = await fetch("http://192.168.0.1/device-id", {
         method: "GET",
         dataType: "JSON"
@@ -25,11 +24,30 @@ export class DeviceIdGetterContainer extends React.Component {
       console.log(`Received Data: ${data}`);
       //response format: {"id":"280020001247353136383631","c":"1"}
       this.setState({ id: data.id });
+      await this.disconnectFromDevice();
     } catch (err) {
       this.setState({
         id:
           "Hmm...Couldn't find your device. Did you connect to it's Wi-Fi network?"
       });
+      console.log(err);
+    }
+  }
+
+  async disconnectFromDevice() {
+    console.log("disconnecting from device...");
+    try {
+      const response = await fetch("http://192.168.0.1/connect-ap", {
+        method: "POST",
+        body: JSON.stringify({ idx: 0 }),
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+          // "Content-Type": "application/x-www-form-urlencoded",
+        }
+      });
+      const data = await response.json();
+      console.log("successfully disconnected");
+    } catch (err) {
       console.log(err);
     }
   }
