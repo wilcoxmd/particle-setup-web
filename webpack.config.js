9837274dc8511257f1d17e517d13c767f2e2d359
Matt Wilcox <wilcox.mattd@gmail.com>
2019-01-31 11:32:33 -0600

    add dev hot reload server

--- webpack.config.js
+++ webpack.config.js
@@ -2,46 +2,52 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 const webpack = require("webpack");
 
-module.exports = {
-  mode: "none",
-  entry: {
-    web: "./src/web-app/index.js",
-    local: "./src/local-app/index.js"
-  },
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "[name].js"
-  },
-  plugins: [
-    new webpack.ProgressPlugin(),
-    new HtmlWebpackPlugin({
-      filename: "local.html",
-      template: "./public/local-setup-file.html",
-      chunks: ["local"]
-    }),
-    new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "./public/index.html",
-      chunks: ["web"]
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
+module.exports = ({ mode } = { mode: "production" }) => {
+  return {
+    mode,
+    entry: {
+      web: "./src/web-app/index.js",
+      local: "./src/local-app/index.js"
+    },
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "[name].js"
+    },
+    plugins: [
+      new webpack.ProgressPlugin(),
+      new HtmlWebpackPlugin({
+        filename: "local.html",
+        template: "./public/local-setup-file.html",
+        chunks: ["local"]
+      }),
+      new HtmlWebpackPlugin({
+        filename: "index.html",
+        template: "./public/index.html",
+        chunks: ["web"]
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader"
+            }
+          ]
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
           }
-        ]
-      },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"]
         }
-      }
-    ]
-  }
+      ]
+    }
+  };
 };
