ee110ceaba2f28916b4b980aebad6943cb3cc418
Matt Wilcox <wilcox.mattd@gmail.com>
2019-02-01 10:47:05 -0600

    add server

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/testlol", (req, res, next) => {
+  res.send(`lol it works lol`);
+});
+
+app.get("/download", (req, res, next) => {
+  fs.readFile("./build/local-setup-file.html", "utf8", function(err, data) {
+    let response = {};
+    response.fileContents = data;
+    res.send(response);
+  });
+});
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
