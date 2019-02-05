7bfc70e40ba89867cb4c8e0ec139c42a15c78fb9
Matt Wilcox <wilcox.mattd@gmail.com>
2019-02-05 10:11:46 -0600

    add file-loader for fonts

--- webpack.config.js
+++ webpack.config.js
@@ -76,6 +76,18 @@ module.exports = (
                 }
               }
             ]
+          },
+          {
+            test: /\.(woff(2)?|ttf|eot|svg|otf)(\?v=\d+\.\d+\.\d+)?$/,
+            use: [
+              {
+                loader: "file-loader",
+                options: {
+                  name: "[name].[ext]",
+                  outputPath: "fonts/"
+                }
+              }
+            ]
           }
         ]
       }
